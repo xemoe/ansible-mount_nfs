dc6389cd26c081faceffdcca22755c1ab87babf0
Teerapong ladlee <teerapong.l@gbtech.co.th>
2016-09-17 18:33:52 +0700

    Fixes and update spec

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -16,4 +16,7 @@ describe "Tasks spec" do
               it { should be_mounted }
         end
     end
+    describe file('/opt/system-config.d/fstab') do
+        its(:content) { should match /172.100.0.202:srv\/nfs \/mnt\/nfs\/172.100.0.202\/nfs nfs4 proto=tcp,port=2049,_netdev 0 0/ }
+    end
 end
